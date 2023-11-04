d495b2dd37f796f647cbd4d1c0a527c73c53cf00
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-11-04 17:00:37 +0100

    Day 50: Implememt Submit button, Handle Post request and Get the data from de user,

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        #data = dict(request.form)
 
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        available_date = request.form["date"]
+        occupation = request.form["occupation"]
+        print(first_name, last_name, email, available_date, occupation)
+
+        #print(data)
+
+    return render_template("index.html")
 
 app.run(debug=True, port=5001)
